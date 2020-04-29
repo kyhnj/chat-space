8f888beaf8fe1f53a07913b7b4bd2009da605fc1
YourName <you@example.com>
2020-04-29 18:26:12 +0900

    グループ、ユーザー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+    # 下の行は削除する
+  # get 'messages/index
 
-  root "messages#index"
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
