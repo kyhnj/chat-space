8872792389854e4263f7ea14d09aa69c294ef063
YourName <you@example.com>
2020-05-14 12:09:08 +0900

    インクリメンタルサーチ

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   # get 'messages/index
 
   root 'groups#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
